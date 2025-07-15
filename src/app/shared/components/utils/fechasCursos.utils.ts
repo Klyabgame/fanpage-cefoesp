233fb0ad839a6eb404fddbb447b2ff96136257e5
unknown <gavino_10@hotmail.es>
2025-07-15 15:09:23 -0500

    fix:new-date-course

--- src/app/shared/components/utils/fechasCursos.utils.ts
+++ src/app/shared/components/utils/fechasCursos.utils.ts
@@ -2,14 +2,14 @@
 export class FechasCursos{
 
     public static fechaCursoBasico={
-        INICIO:'07/07/2025',
-        FIN:'12/07/2025',
+        INICIO:'18/07/2025',
+        FIN:'25/07/2025',
         HORARIO:'08:00 AM - 15:30 PM'
     }
 
     public static fechaCursoPerfeccionamiento={
-        INICIO:'14/07/2025',
-        FIN:'17/07/2025',
+        INICIO:'26/07/2025',
+        FIN:'01/08/2025',
         HORARIO:'08:00 AM - 15:30 PM'
     }
 }
